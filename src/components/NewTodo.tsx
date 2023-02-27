fd3cc174a02a2ac346ec1a3f4b6f4541a7915327
alirezayoom <alirezayoom@gmail.com>
2023-02-27 19:05:20 +0430

    submitHandler added

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,8 +1,22 @@
+import { useRef } from "react";
+
 export default function NewTodo() {
+  const textRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const newText = textRef.current!.value;
+    console.log(newText);
+  };
+
   return (
-    <form className="flex space-x-2 py-8">
+    <form onSubmit={submitHandler} className="flex space-x-2 py-8">
       <div>
-        <input type="text" className="border-2 border-gray-900 px-4 py-1" />
+        <input
+          type="text"
+          ref={textRef}
+          className="border-2 border-gray-900 px-4 py-1"
+        />
       </div>
       <button type="submit" className="border-2 border-gray-900 px-4 py-1">
         ADD TODO
